e785e52bb02be9bdda9d76b552884733eb833eef
Zionncube <hncube1@byupathway.com>
2025-03-30 10:47:34 -0700

    w04 join and thakyou page.

--- chamber/scripts/dates.js
+++ chamber/scripts/dates.js
@@ -4,4 +4,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Set last modification date
     document.getElementById("last-modified").textContent = document.lastModified;
-})
+})// Set the timestamp value to the current date and time in ISO format
+
+
+// This section is for the  join page for join and thankyou page
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("timestamp").value = new Date().toISOString();
+});
+
+document.getElementById('timestamp').textContent = params.get('timestamp') || 'N/A';
+
+
+
